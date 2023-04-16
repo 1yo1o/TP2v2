4eaa0282dd26c5621646f22dc80a47b2050411db
1yo1o <brochneedragule@gmail.com>
2023-04-16 12:08:12 -0400

    table sort of work

--- src/main/java/com/example/tp2v2/ClientFX.java
+++ src/main/java/com/example/tp2v2/ClientFX.java
@@ -2,11 +2,13 @@ package com.example.tp2v2;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.geometry.Insets;
+import javafx.scene.control.cell.MapValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -21,12 +23,16 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ClientFX extends Application {
     public static ArrayList<Course> listeCourse = new ArrayList<Course>();
+    public static TableView tableView;
     public final static String REGISTER_COMMAND = "INSCRIRE";
     public final static String LOAD_COMMAND = "CHARGER";
+    public static String listeErreur;
     @Override
     public void start(Stage stage) throws IOException {
         HBox root = new HBox();
@@ -41,20 +47,30 @@ public class ClientFX extends Application {
         texteListe.setTextAlignment(TextAlignment.CENTER);
 
         gauche.getChildren().add(texteListe);
-
-        TableView tableView = new TableView();
-        TableColumn<Course, String> column1 =
+        ChargerCours("Automne");
+        tableView = new TableView();
+        TableColumn<Map, String> column1 =
                 new TableColumn<>("Code");
-        TableColumn<Course, String> column2 =
+        column1.setCellValueFactory(new MapValueFactory<>("Code"));
+        column1.setSortable(false);
+        TableColumn<Map, String> column2 =
                 new TableColumn<>("Cours");
+        column2.setCellValueFactory(new MapValueFactory<>("Cours"));
+        column2.setSortable(false);
         tableView.getColumns().add(column1);
         tableView.getColumns().add(column2);
+        tableView = Update(tableView);
 
+
+
+
+        tableView.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
         gauche.getChildren().add(tableView);
 
         HBox bas = new HBox();
         gauche.getChildren().add(bas);
         ChoiceBox cb = new ChoiceBox(FXCollections.observableArrayList("Automne", "Hiver", "Ete"));
+        cb.setValue("Automne");
         Button chargerCours = new Button("charger");
         bas.getChildren().addAll(cb, chargerCours);
 
@@ -85,14 +101,15 @@ public class ClientFX extends Application {
 
         chargerCours.setOnAction((event) ->
         {
-            //String a = infoPrenom.getText();
             ChargerCours(cb.getValue().toString());
+            tableView = Update(tableView);
+            //String a = infoPrenom.getText();
         });
         stage.setScene(scene);
 
         envoyerInfo.setOnAction((event) -> {
 
-            Inscription(infoPrenom.getText(), infoNom.getText(), infoEmail.getText(), infoMatricule.getText());
+            Inscription(infoPrenom.getText(), infoNom.getText(), infoEmail.getText(), infoMatricule.getText(),0);
         });
 
         stage.show();
@@ -110,10 +127,6 @@ public class ClientFX extends Application {
             ObjectInputStream is = new ObjectInputStream(clientSocket.getInputStream());
             listeCourse = (ArrayList<Course>) is.readObject();
         }
-        catch (NullPointerException ex)
-        {//Aucune session choisit
-            ex.printStackTrace();
-        }
         catch (IOException ex)
         {
             ex.printStackTrace();
@@ -123,11 +136,11 @@ public class ClientFX extends Application {
             ex.printStackTrace();
         }
     }
-    private void Inscription(String prenom, String nom, String email, String matricule)
+    private void Inscription(String prenom, String nom, String email, String matricule, int index)
     {
         try
         {
-            Course cours = listeCourse.get(0);
+            Course cours = listeCourse.get(index);
             RegistrationForm coursInscrit = new RegistrationForm(prenom, nom, email, matricule, cours);
 
             Socket clientSocket = new Socket("127.0.0.1", 1337);
@@ -144,6 +157,21 @@ public class ClientFX extends Application {
         {
             System.out.println("No session selected");
         }
+    }
+    private TableView Update(TableView tableView)
+    {
+        tableView.getItems().clear();
+        ObservableList<Map<String, Object>> items = FXCollections.<Map<String, Object>>observableArrayList();
+        for(int i = 0; i<listeCourse.size(); ++i)
+        {
+            Map<String, Object> item = new HashMap<>();
+            item.put("Code", listeCourse.get(i).getCode());
+            item.put("Cours", listeCourse.get(i).getName());
+            items.add(item);
+        }
+        tableView.getItems().addAll(items);
+        tableView.refresh();
+        return tableView;
     }
 
     public static void main(String[] args) {
